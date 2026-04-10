fde90653c8332a97bcad8c60a0be3ccb456273cc
Okara008 <smileotito123@gmail.com>
2026-04-10 17:28:43 +0100

    editing tutorial page

--- src/Tutorial.jsx
+++ src/Tutorial.jsx
@@ -10,43 +10,45 @@ function Tutorial({wasClicked, removeTutorial}) {
     const tutorial_display_ref = useRef([
         {img: tutorial_roll, info: "Hit the Roll button to play → You can roll up to 3 times on your turn."},
         {img: tutorial_tool_tip, info: "Hover over / tap each scoring box to learn how points are calculated."},
-        {img: tutorial_confirm_score, info: "Tap the box with your color to preview your points → Hit PLAY to keep them."},
         {img: tutorial_lock_dice, info: "Tap dice to lock them → locked dice won't change on next roll."},
+        {img: tutorial_confirm_score, info: "Tap the box with your color to preview your points → Hit PLAY to record that score for your turn."},
+        {img: "", info: "No need to use all three rolls. Stop early if you already like your dice!"},
         {img: tutorial_score_board, info: "Your total score appears here on the scoreboard."},
         {img: "", info: "After every cell is played, a winner is announced. If scores are tied, it's a draw."},
     ]) 
-    const [tutorial_display, set_tutorial_display] = useState([...tutorial_display_ref.current])
+    const [tutorial_display] = useState([...tutorial_display_ref.current])
     const [disable_btn, set_disable_btn] = useState({prev: true, next: false})
     const index_of_display_ref = useRef(0)
     const [index_of_display, set_index_of_display] = useState(index_of_display_ref.current)
 
     const update_index = (num) => {
-        index_of_display_ref.current = (index_of_display_ref.current + num ) % tutorial_display_ref.current.length
+        index_of_display_ref.current = index_of_display_ref.current + num
         set_index_of_display(index_of_display_ref.current)
 
         set_disable_btn(pre => ({...pre, next: (index_of_display_ref.current === (tutorial_display_ref.current.length - 1) ? true : false)}))
         set_disable_btn(pre => ({...pre, prev: (index_of_display_ref.current === (0) ? true : false)}))
-
-        
     }
+
     return(<>
-        {wasClicked && ( <div className="outerShell tutorial_page">
-            <section className="tutorialSection">
-                <img src={x_button} alt="x" className="x_button" onClick={removeTutorial}/>
-                <div className="tutorial_img_display">
-                    <small id="page_num">{index_of_display + 1}</small>
-                    {
-                        index_of_display != (tutorial_display_ref.current.length - 1) &&
-                        (<img src={tutorial_display[index_of_display].img} alt="Tutorial Image" id='display_img'/>)
-                    }
-                    <p className="tutorial_info">{tutorial_display[index_of_display].info}</p>
-                </div>
-                <div className="tutorial_btns">
-                    <button disabled={disable_btn.prev} id='tutorial_Previous' onClick={() => update_index(-1)}>Previous</button>
-                    <button disabled={disable_btn.next} id='tutorial_Next' onClick={() => update_index(1)}>Next</button>
-                </div>
-            </section>
-        </div>)}
+        {wasClicked && ( 
+            <div className="outerShell tutorial_page">
+                <section className="tutorialSection">
+                    <img src={x_button} alt="x" className="x_button" onClick={removeTutorial}/>
+                    <div className="tutorial_img_display">
+                        <small id="page_num">{index_of_display + 1}</small>
+                        {
+                            tutorial_display[index_of_display].img &&
+                            (<img src={tutorial_display[index_of_display].img} alt="Tutorial Image" id='display_img'/>)
+                        }
+                        <p className="tutorial_info">{tutorial_display[index_of_display].info}</p>
+                    </div>
+                    <div className="tutorial_btns">
+                        <button disabled={disable_btn.prev} id='tutorial_Previous' onClick={() => update_index(-1)}>Previous</button>
+                        <button disabled={disable_btn.next} id='tutorial_Next' onClick={() => update_index(1)}>Next</button>
+                    </div>
+                </section>
+            </div>
+        )}
     </>)
 }
 
