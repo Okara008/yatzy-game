f10214445d279768f6be95c16828f5a5294984e2
Okara008 <smileotito123@gmail.com>
2026-04-15 08:09:39 +0100

    Modifing tutorial page

--- src/Tutorial.jsx
+++ src/Tutorial.jsx
@@ -3,16 +3,20 @@ import tutorial_confirm_score from './assets/tutorial_confirm_score.png'
 import tutorial_lock_dice from './assets/tutorial_lock_dice.png'
 import tutorial_score_board from './assets/tutorial_score_board.png'
 import tutorial_tool_tip from './assets/tutorial_tool_tip.png'
+import tutorial_play_btn from './assets/tutorial_play_btn.png'
 import x_button from './assets/x_icon.png'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 function Tutorial({wasClicked, removeTutorial}) {
     const tutorial_display_ref = useRef([
         {img: tutorial_roll, info: "Hit the Roll button to play → You can roll up to 3 times on your turn."},
         {img: tutorial_lock_dice, info: "Tap dice to lock them → locked dice won't change on next roll."},
         {img: "", info: "No need to use all three rolls. Stop early if you're satisfied with your dice!"},
+        {img: "", info: "Each box on the board is a different scoring category. Ones, Twos, Threes, Four, Fives, Sixes — plus special combinations like '3 of a kind' and 'Full House'."},
         {img: tutorial_tool_tip, info: "Hover over / tap each scoring box to learn how points are calculated."},
-        {img: tutorial_confirm_score, info: "Tap the box with your color to preview your points → Hit PLAY to record that score for your turn."},
+        {img: tutorial_confirm_score, info: "Tap the box with your color to preview your points"},
+        {img: tutorial_play_btn, info: "Hit PLAY to record your score."},
+        {img: "", info: "Once you record a score in a box, you cannot use it again. Choose wisely!"},
         {img: tutorial_score_board, info: "Your total score appears here on the scoreboard."},
         {img: "", info: "After every cell is played, a winner is announced. If scores are tied, it's a draw."},
     ]) 
@@ -27,7 +31,12 @@ function Tutorial({wasClicked, removeTutorial}) {
         set_disable_btn(pre => ({...pre, next: (index_of_display_ref.current === (tutorial_display_ref.current.length - 1) ? true : false)}))
         set_disable_btn(pre => ({...pre, prev: (index_of_display_ref.current === (0) ? true : false)}))
     }
-
+    
+    useEffect(() => {
+        index_of_display_ref.current = 0
+        set_index_of_display(index_of_display_ref.current)
+        set_disable_btn({prev: true, next: false})
+    }, [wasClicked])
     return(<>
         {wasClicked && ( 
             <div className="outerShell tutorial_page">
